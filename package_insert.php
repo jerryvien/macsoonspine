2080f173d7febd5a6ce10570ae62357d843ce61f
jerryvien <jerryyee0902@gmail.com>
2024-11-04 00:04:18 +0800

    asd

--- package_insert.php
+++ package_insert.php
@@ -38,8 +38,9 @@ try {
     <!-- Include Select2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <!-- Include Select2 JS -->
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    
 </head>
 <body>
 
