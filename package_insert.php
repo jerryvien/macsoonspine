560435fad6dd5a60dc42bdce9fc903b0577dcdf2
jerryvien <jerryyee0902@gmail.com>
2024-11-04 00:03:04 +0800

    asd

--- package_insert.php
+++ package_insert.php
@@ -37,6 +37,9 @@ try {
     <link href="assets/css/style.css" rel="stylesheet">
     <!-- Include Select2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <!-- Include Select2 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
 
@@ -210,9 +213,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/config/footer.php';
 <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="assets/js/main.js"></script>
 
-<!-- Include Select2 JS -->
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 
 </body>
 </html>
